58152dd7ab3f9b6032913c761f905527b8f2c864
DanishVaid <danishvaid@umail.ucsb.edu>
2018-04-12 22:52:18 -0700

    dv - adding firstRun Dialog section

--- app.js
+++ app.js
@@ -79,9 +79,33 @@ var bot = new builder.UniversalBot(connector, [
         session.send(session.message.text);
     
 }]);
-//bot.set('storage', tableStorage);
-/*
-bot.dialog('/', function (session) {
-    session.send('You said ' + session.message.text);
+// bot.set('storage', tableStorage);
+
+// Add first run dialog
+bot.dialog('firstRun', [
+    function (session) {
+        // Update versio number and start Prompts
+        // - The version number needs to be updated first to prevent re-triggering 
+        //   the dialog. 
+        session.userData.version = 1.0; 
+        builder.Prompts.text(session, "Hello, setting up ... What's your name?");
+    },
+    function (session, results) {
+        // We'll save the users name and send them an initial greeting. All 
+        // future messages from the user will be routed to the root dialog.
+        session.userData.name = results.response;
+        session.endDialog("Hi %s, say something to me and I'll echo it back.", session.userData.name); 
+    }
+]).triggerAction({
+    onFindAction: function (context, callback) {
+        // Trigger dialog if the users version field is less than 1.0
+        // - When triggered we return a score of 1.1 to ensure the dialog is always triggered.
+        var ver = context.userData.version || 0;
+        var score = ver < 1.0 ? 1.1: 0.0;
+        callback(null, score);
+    },
+    onInterrupted: function (session, dialogId, dialogArgs, next) {
+        // Prevent dialog from being interrupted.
+        session.send("Sorry... We need some information from you first.");
+    }
 });
-*/
