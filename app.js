03bef578ab25bd39731c3261661bbda73ba8ee94
pfeghali <peterfeghali99@gmail.com>
2018-04-13 00:52:02 -0700

    zombie

--- app.js
+++ app.js
@@ -34,38 +34,13 @@ var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azu
 var emergency;
 var location;
 
-const Browser = require('zombie');
 
-// We're going to make requests to http://example.com/signup
-// Which will be routed to our test server localhost:3000
-Browser.localhost('example.com', 3000);
+var Browser = require("zombie");
+var url = "http://work.krasimirtsonev.com/git/blog-posts/TestingWithZombieJS/site/";
+var browser = new Browser();
 
-describe('User visits signup page', function() {
+describe("testing with zombie", function() {
 
-  const browser = new Browser();
-
-  before(function(done) {
-    browser.visit('/signup', done);
-  });
-
-  describe('submits form', function() {
-
-    before(function(done) {
-      browser
-        .fill('email',    'zombie@underworld.dead')
-        .fill('password', 'eat-the-living')
-        .pressButton('Sign Me Up!', done);
-    });
-
-    it('should be successful', function() {
-      browser.assert.success();
-    });
-
-    it('should see welcome page', function() {
-      browser.assert.text('title', 'Welcome To Brains Depot');
-      console.log("success")
-    });
-  });
 });
 
 
