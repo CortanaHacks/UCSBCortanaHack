69c0774462d7f36a06fc4bd18c4488e3f527663a
Abhishek Bhattacharya <abbirdboy@Abhisheks-MacBook-Pro.local>
2018-04-13 02:21:11 -0700

    get name?

--- app.js
+++ app.js
@@ -37,60 +37,22 @@ var location;
 // Create your bot with a function to receive messages from the user
 
 // var bot = new builder.UniversalBot(connector, []);
+var inMemoryStorage = new builder.MemoryBotStorage();
 
-var userStore = [];
-var bot = new builder.UniversalBot(connector, function (session) {
-    // store user's address
-    var address = session.message.address;
-    userStore.push(address);
-
-    // end current dialog
-    session.endDialog('You\'ve been invited to a survey! It will start in a few seconds...');
-});
-
-// Every 5 seconds, check for new registered users and start a new dialog
-setInterval(function () {
-    var newAddresses = userStore.splice(0);
-    newAddresses.forEach(function (address) {
-
-        console.log('Starting survey for address:', address);
-
-        // new conversation address, copy without conversationId
-        var newConversationAddress = Object.assign({}, address);
-        delete newConversationAddress.conversation;
-
-        // start survey dialog
-        bot.beginDialog(newConversationAddress, 'survey', null, function (err) {
-            if (err) {
-                // error ocurred while starting new conversation. Channel not supported?
-                bot.send(new builder.Message()
-                    .text('This channel does not support this operation: ' + err.message)
-                    .address(address));
-            }
-        });
-
-    });
-}, 5000);
-
-bot.dialog('survey', [
+var bot = new builder.UniversalBot(connector, [
     function (session) {
-        builder.Prompts.text(session, 'Hello... What\'s your name?');
+        session.say("Hi! I am Ella, your EMS assistant. I need to ask you a few basic questions to setup.");
+        builder.Prompts.text(session, 'What is your name?', {                                    
+            speak: 'What is name?',                                               
+            retrySpeak: "I'm sorry, please repeat your name",  
+            inputHint: builder.InputHint.expectingInput                                              
+        });
     },
     function (session, results) {
         session.userData.name = results.response;
-        builder.Prompts.number(session, 'Hi ' + results.response + ', How many years have you been coding?');
-    },
-    function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, 'What language do you code Node using? ', ['JavaScript', 'CoffeeScript', 'TypeScript']);
-    },
-    function (session, results) {
-        session.userData.language = results.response.entity;
-        session.endDialog('Got it... ' + session.userData.name +
-            ' you\'ve been programming for ' + session.userData.coding +
-            ' years and use ' + session.userData.language + '.');
+        session.save();
     }
-]);
+]).set('storage', inMemoryStorage); // Register in-memory storage ;
 //bot.set('storage', tableStorage);
 
 // bot.dialog('/', function (session) {
