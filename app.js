6438a7a3245b2970f700cda33ac70809c07db540
pfeghali <peterfeghali99@gmail.com>
2018-04-13 00:46:56 -0700

    Dual versions + fix

--- app.js
+++ app.js
@@ -34,6 +34,44 @@ var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azu
 var emergency;
 var location;
 
+const Browser = require('zombie');
+
+// We're going to make requests to http://example.com/signup
+// Which will be routed to our test server localhost:3000
+Browser.localhost('example.com', 3000);
+
+describe('User visits signup page', function() {
+
+  const browser = new Browser();
+
+  before(function(done) {
+    browser.visit('/signup', done);
+  });
+
+  describe('submits form', function() {
+
+    before(function(done) {
+      browser
+        .fill('email',    'zombie@underworld.dead')
+        .fill('password', 'eat-the-living')
+        .pressButton('Sign Me Up!', done);
+    });
+
+    it('should be successful', function() {
+      browser.assert.success();
+    });
+
+    it('should see welcome page', function() {
+      browser.assert.text('title', 'Welcome To Brains Depot');
+      console.log("success")
+    });
+  });
+});
+
+
+
+
+
 // Create your bot with a function to receive messages from the user
 
 var bot = new builder.UniversalBot(connector, [
@@ -86,7 +124,7 @@ var bot = new builder.UniversalBot(connector, [
 
     
         session.send(msg);
-        builder.Prompts.text(session, "What's your name? " + location);
+        builder.Prompts.text(session, "What are your symptoms?");
         
 },
     function(session, results){
